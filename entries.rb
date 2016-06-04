685cded427184aead8dbdd3d38eac60b887c380c
Michael Goodwin <michael.goodwin@thescore.com>
2016-06-03 21:03:39 -0400

    Add 2016 results

--- entries.rb
+++ entries.rb
@@ -39,6 +39,9 @@ cathy:     { name: "Cathy Lemon",      sex: "Female", soo: "10051944",
                "Variety Village Spring 2015" => { "100 Free" => 161.68, "25 Fly" => 30, "50 Breast" => 102.72, "100 IM" => 195.18 }, # DQ in 25 Fly
                "Provincial Qualifier 2015" => { "25 Fly" => 42.09, "100 IM" => 181.96, "50 Free" => 71.88, "50 Back" => 91.28, "100 Free" => 163.57, "50 Breast" => 98.63 },
                "Mike Conrad Winter 2016" => { "100 IM" => 183.15, "25 Breast" => 45.39, "50 Back" => 89.91, "50 Free" => 73.5 },
+               "Sarah Carman Spring 2016" => { "25 Breast" => 45.49, "50 Back" => 87.62, "50 Free" => 76.36, "100 IM" => 182.68 },
+               "Variety Village Spring 2016" => { "200 Free" => 345.13, "25 Breast" => 47.26, "50 Back" => 90.45, "25 Free" => 33.54 },
+               "Anne Ottenbrite 2016" => { "200 Free" => 349.22, "25 Breast" => 47.4, "50 Back" => 91.87, "25 Free" => 34.16, "25 Back" => 40.89, "50 Free" => 77.44 },
              }
            },
 daniel:    { name: "Daniel Goodman",   sex: "Male",   soo: "10047780",
@@ -46,11 +49,7 @@ daniel:    { name: "Daniel Goodman",   sex: "Male",   soo: "10047780",
              pbs: { "50 Free" => 55, "25 Back" => 36, "25 Breast" => 43, "25 Free" => 27, "25 Fly" => 32 },
              dob: "24/03/1986",
              results: {
-               "Variety Village Fall 2014" => {  },
-               "Mike Conrad Winter 2015" => {  },
-               "Variety Village Spring 2015" => {  },
                "Provincial Qualifier 2015" => { "25 Free" => 17.53, "25 Fly" => 20.45, "50 Free" => 37.86, "50 Fly" => 49.51, "25 Back" => 21.38, "25 Breast" => 25.68 },
-               "Mike Conrad Winter 2016" => {  },
              }
            },
 dj:        { name: "David Beaton",     sex: "Male",   soo: "10041460",
@@ -61,7 +60,7 @@ dj:        { name: "David Beaton",     sex: "Male",   soo: "10041460",
                "Variety Village Fall 2014" => { "50 Free" => 51.99, "100 Free" => 133.16, "50 Back" => 73.33, "25 Breast" => 37.38 }, # DQ in 25 Breast
                "Mike Conrad Winter 2015" => { "50 Free" => 62.84, "25 Free" => 22, "50 Back" => 72.54, "25 Breast" => 35.78 },
                "Variety Village Spring 2015" => { "50 Free" => 56.81, "25 Free" => 24.25, "50 Back" => 70.61, "25 Breast" => 36.66 }, # DQ in 25 Breast
-               "Mike Conrad Winter 2016" => {  },
+               "Variety Village Spring 2016" => { "50 Free" => 49.91, "25 Free" => 21.96, "50 Back" => 66.59, "25 Back" => 30.64 },
              }
            },
 duncan:    { name: "Duncan Fong",      sex: "Male",   soo: "10112524",
@@ -69,10 +68,9 @@ duncan:    { name: "Duncan Fong",      sex: "Male",   soo: "10112524",
              pbs: { "25 Free" => 43, "50 Free" => 125, "25 Back" => 58, "50 Back" => 135 },
              dob: "29/07/1992",
              results: {
-               "Variety Village Fall 2014" => {  },
-               "Mike Conrad Winter 2015" => {  },
                "Variety Village Spring 2015" => { "25 Free" => 44.31, "50 Free" => 114.83, "25 Back" => 63.63},
-               "Mike Conrad Winter 2016" => {  },
+               "Sarah Carman Spring 2016" => { "25 Free" => 53.04, "50 Free" => 119.84, "25 Back" => 71.49, "50 Back" => 144.56 },
+               "Anne Ottenbrite 2016" => { "25 Free" => 48.79, "50 Free" => 123.45, "25 Back" => 70.39, "50 Back" => 165.35 }
              }
            },
 gord:      { name: "Gordon Hefford",   sex: "Male",   soo: "10048885",
@@ -84,7 +82,6 @@ gord:      { name: "Gordon Hefford",   sex: "Male",   soo: "10048885",
                "Mike Conrad Winter 2015" => { "50 Free" => 75.81, "50 Back" => 82.28, "25 Fly" => 54.84, "100 IM" => 211.12 },
                "Variety Village Spring 2015" => { "50 Free" => 71.31, "50 Back" => 86.43, "25 Fly" => 45.85, "100 IM" => 218.62 }, # DQ in 50 Back, 100 IM
                "Provincial Qualifier 2015" => { "50 Free" => 72.06, "50 Back" => 88.51, "25 Breast" => 54.54 },
-               "Mike Conrad Winter 2016" => {  },
              }
            },
 hilda:     { name: "Hilda Wong",       sex: "Female", soo: "10062297",
@@ -96,6 +93,9 @@ hilda:     { name: "Hilda Wong",       sex: "Female", soo: "10062297",
                "Mike Conrad Winter 2015" => { "25 Free" => 31.54, "50 Free" => 71.69, "25 Back" => 45.52, "25 Breast" => 48.36 },
                "Variety Village Spring 2015" => { "25 Free" => 30.62, "50 Free" => 72.66, "25 Back" => 46.47, "25 Breast" => 48.14 },
                "Mike Conrad Winter 2016" => { "25 Free" => 33.75, "50 Free" => 79.24, "25 Back" => 45.77, "50 Back" => 83.94 },
+               "Sarah Carman Spring 2016" => { "25 Free" => 36.56, "50 Free" => 80.51, "25 Back" => 38.95, "50 Back" => 83.4 },
+               "Variety Village Spring 2016" => { "25 Free" => 34.21, "50 Free" => 80.63, "25 Back" => 45.32, "50 Back" => 83.65 }, # DQ in 25 Back
+               "Anne Ottenbrite 2016" => { "25 Free" => 35.09, "50 Free" => 81.16, "25 Back" => 38.2, "50 Back" => 84.23 },
              }
            },
 james:     { name: "James Brown",      sex: "Male",   soo: "10095198",
@@ -108,6 +108,9 @@ james:     { name: "James Brown",      sex: "Male",   soo: "10095198",
                "Variety Village Spring 2015" => { "25 Free" => 47.38, "50 Back" => 103.21, "25 Back" => 37.26, "25 Breast" => 50.12 },
                "Provincial Qualifier 2015" => { "50 Free" => 65.53, "50 Back" => 80.45, "25 Back" => 37.31, "25 Breast" => 42.89 },
                "Mike Conrad Winter 2016" => { "25 Free" => 25.63, "50 Back" => 67.49, "25 Breast" => 36.9, "50 Free" => 58.38 },
+               "Sarah Carman Spring 2016" => { "25 Free" => 24.79, "50 Back" => 82.91, "25 Breast" => 36.73, "50 Free" => 57.31 },
+               "Variety Village Spring 2016" => { "25 Free" => 24.52, "50 Back" => 65.31, "25 Breast" => 36.89, "50 Free" => 51.49 }, # DQ in 25 Breast
+               "Anne Ottenbrite 2016" => { "25 Free" => 24.47, "50 Back" => 65.88, "25 Breast" => 34.1, "50 Free" => 58, "25 Back" => 34.61, "50 Breast" => 80.58 },
              }
            },
 jeffc:     { name: "Jeffrey Campbell", sex: "Male",   soo: "10043196",
@@ -120,6 +123,8 @@ jeffc:     { name: "Jeffrey Campbell", sex: "Male",   soo: "10043196",
                "Variety Village Spring 2015" => { "100 Free" => 97.36, "25 Free" => 18.49, "50 Breast" => 65.02, "50 Fly" => 54.21 },
                "Provincial Qualifier 2015" => { "25 Free" => 16.58, "100 IM" => 112.83, "50 Fly" => 52.05, "25 Back" => 21.32, "100 Free" => 102.52, "50 Breast" => 60.70 },
                "Mike Conrad Winter 2016" => { "25 Free" => 16.84, "50 Breast" => 67.91, "50 Fly" => 55.33, "50 Back" => 46.87 }, # DQ in 50 Breats (kick)
+               "Sarah Carman Spring 2016" => { "50 Free" => 37.9, "25 Fly" => 21.47, "50 Back" => 52.58, "100 IM" => 120.32, "25 Back" => 20.44 },
+               "Variety Village Spring 2016" => { "50 Fly" => 61.88, "50 Free" => 36.7, "25 Fly" => 22.14, "50 Back" => 50.61 },
              }
            },
 jeffv:     { name: "Jeffrey Vanier",   sex: "Male",   soo: "10061088",
@@ -127,11 +132,10 @@ jeffv:     { name: "Jeffrey Vanier",   sex: "Male",   soo: "10061088",
              pbs: { "25 Free" => 33, "50 Free" => 80, "25 Back" => 55 },
              dob: "02/08/1987",
              results: {
-               "Variety Village Fall 2014" => {  },
                "Mike Conrad Winter 2015" => { "50 Back" => 112.38, "50 Free" => 69.53, "25 Back" => 53.31 },
-               "Variety Village Spring 2015" => {  },
                "Provincial Qualifier 2015" => { "25 Free" => 27.59, "50 Free" => 69.61, "50 Back" => 102.65, "25 Back" => 50.69 },
-               "Mike Conrad Winter 2016" => {  },
+               "Variety Village Spring 2016" => { "25 Free" => 34.74, "50 Free" => 71.33, "25 Back" => 77.59, "50 Back" => 131.53 },
+               "Anne Ottenbrite 2016" => { "25 Free" => 32.37, "50 Free" => 73.56, "25 Back" => 62.53, "50 Back" => 120.25 }
              }
            },
 jessica:   { name: "Jessica Nishio",   sex: "Female", soo: "10144952",
@@ -151,8 +155,6 @@ joey:      { name: "Joseph Carnovale", sex: "Male",   soo: "10043325",
              results: {
                "Variety Village Fall 2014" => { "25 Free" => 36.75, "50 Free" => 99.43, "25 Back" => 83.24 }, # DQ in 25 Back
                "Mike Conrad Winter 2015" => { "25 Free" => 36.84, "50 Free" => 88.08, "25 Back" => 74.12 },
-               "Variety Village Spring 2015" => {  },
-               "Mike Conrad Winter 2016" => {  },
              }
            },
 lindsay:   { name: "Lindsay Suline",   sex: "Female", soo: "10148007",
@@ -165,6 +167,9 @@ lindsay:   { name: "Lindsay Suline",   sex: "Female", soo: "10148007",
                "Variety Village Spring 2015" => { "25 Free" => 30.31, "50 Free" => 69.27, "25 Back" => 39.31, "25 Breast" => 57.05},
                "Provincial Qualifier 2015" => { "25 Free" => 28.03, "50 Free" => 67.42, "50 Back" => 75.16, "25 Back" => 33.58, "25 Breast" => 61.11, "50 Breast" => 123.44 }, # DQ in 25 Breast, 50 Breast
                "Mike Conrad Winter 2016" => { "25 Free" => 30.87, "50 Free" => 64.31, "25 Breast" => 49.93, "50 Back" => 77.26 },
+               "Sarah Carman Spring 2016" => { "25 Free" => 31.4, "50 Free" => 68.73, "25 Breast" => 54.86, "50 Back" => 80.17, "100 IM" => 201.27, "50 Breast" => 117.77 },
+               "Variety Village Spring 2016" => { "25 Free" => 30.37, "50 Free" => 66.98, "25 Breast" => 63.17, "50 Back" => 78.28 },
+               "Anne Ottenbrite 2016" => { "25 Free" => 31.2, "50 Free" => 64.76, "25 Breast" => 67.71, "50 Back" => 84.76, "100 IM" => 207.14, "25 Back" => 39.64 },
              }
            },
 marco:     { name: "Marco Fata",       sex: "Male",   soo: "10046465",
@@ -177,6 +182,9 @@ marco:     { name: "Marco Fata",       sex: "Male",   soo: "10046465",
                "Variety Village Spring 2015" => { "25 Free" => 31.15, "50 Free" => 77.54, "25 Back" => 40.3, "50 Back" => 81.3 },
                "Provincial Qualifier 2015" => { "50 Free" => 75.35, "50 Back" => 85.70, "25 Back" => 39.37 },
                "Mike Conrad Winter 2016" => { "25 Free" => 33.47, "50 Free" => 78.27, "25 Back" => 35.32, "50 Back" => 87.43 },
+               "Sarah Carman Spring 2016" => { "25 Free" => 32.68, "50 Free" => 72.92, "25 Back" => 36.79, "50 Back" => 84.18 },
+               "Variety Village Spring 2016" => { "25 Free" => 29.88, "50 Free" => 72.73, "25 Back" => 38.46, "50 Back" => 81.08 },
+               "Anne Ottenbrite 2016" => { "25 Free" => 30.99, "50 Free" => 77.12, "25 Back" => 41.99, "50 Back" => 86.53 },
              }
            },
 marianna:  { name: "Marianna Adams",   sex: "Female", soo: "10040299",
@@ -220,6 +228,7 @@ pete:      { name: "Peter Flagler",    sex: "Male",   soo: "10046714",
                "Variety Village Spring 2015" => { "25 Free" => 22.01, "100 Free" => 107, "25 Back" => 26.15, "25 Fly" => 42.61 },
                "Provincial Qualifier 2015" => { "25 Free" => 19.98, "50 Free" => 42.24, "50 Back" => 58.14, "25 Back" => 25.13, "100 Free" => 102.74 },
                "Mike Conrad Winter 2016" => { "25 Free" => 21.36, "100 Free" => 110.6, "25 Back" => 26.16 },
+               "Sarah Carman Spring 2016" => { "25 Free" => 20.46, "50 Free" => 44.97, "100 Free" => 108.6, "25 Back" => 65.52 },
              }
            },
 rick:      { name: "Rick Wong",        sex: "Male",   soo: "10062303",
@@ -230,6 +239,9 @@ rick:      { name: "Rick Wong",        sex: "Male",   soo: "10062303",
                "Mike Conrad Winter 2015" => { "25 Free" => 63.66, "50 Free" => 144.72, "25 Back" => 63.68, "50 Back" => 144.72 },
                "Variety Village Spring 2015" => { "25 Free" => 85.04, "50 Free" => 150.23, "25 Back" => 64.02, "50 Back" => 152.01 },
                "Mike Conrad Winter 2016" => { "25 Free" => 76.52, "50 Free" => 143.34, "25 Back" => 62.22, "50 Back" => 140.83 },
+               "Sarah Carman Spring 2016" => { "25 Free" => 68.31, "50 Free" => 149.61, "25 Back" => 58.77, "50 Back" => 128.9 },
+               "Variety Village Spring 2016" => { "25 Free" => 65.55, "50 Free" => 152.48, "25 Back" => 58.78, "50 Back" => 135.96 },
+               "Anne Ottenbrite 2016" => { "25 Free" => 72.56, "50 Free" => 147.26 },
              }
            },
 sarah:     { name: "Sarah Bruni",      sex: "Female", soo: "10042779",
@@ -240,6 +252,7 @@ sarah:     { name: "Sarah Bruni",      sex: "Female", soo: "10042779",
                "Variety Village Spring 2015" => { "25 Free" => 33.62, "50 Free" => 86.28, "25 Back" => 63.46},
                "Provincial Qualifier 2015" => { "50 Free" => 86.28, "50 Back" => 134.19, "25 Back" => 66.61 },
                "Mike Conrad Winter 2016" => { "25 Free" => 39.74, "50 Free" => 100.9, "25 Back" => 53.15, "50 Back" => 122.75 },
+               "Sarah Carman Spring 2016" => { "25 Free" => 42.34, "50 Free" => 97.5, "25 Back" => 74.17, "50 Back" => 127.77 },
              }
            },
 steven:    { name: "Steven Kennedy",   sex: "Male",   soo: "10050597",
@@ -250,6 +263,7 @@ steven:    { name: "Steven Kennedy",   sex: "Male",   soo: "10050597",
                "Mike Conrad Winter 2015" => { "25 Free" => 38.12, "50 Free" => 110.91, "25 Back" => 41.72, "50 Back" => 104.76 },
                "Variety Village Spring 2015" => { "25 Free" => 41.26, "50 Free" => 96.72, "25 Back" => 46.64, "50 Back" => 95.68 },
                "Mike Conrad Winter 2016" => { "25 Free" => 47.91, "50 Free" => 106.68, "25 Back" => 46.09, "50 Back" => 101.01 },
+               "Variety Village Spring 2016" => { "25 Free" => 46.94, "50 Free" => 111.6, "25 Back" => 41.83, "50 Back" => 107.12 },
              }
            },
 tasia:     { name: "Tasia Mazomenos",  sex: "Female", soo: "10053230",
@@ -258,6 +272,7 @@ tasia:     { name: "Tasia Mazomenos",  sex: "Female", soo: "10053230",
              dob: "25/08/1972",
              results: {
                "Mike Conrad Winter 2016" => { "25 Free" => 91.97, "50 Free" => 152.04, "25 Back" => 91.91, "50 Back" => 180.9 },
+               "Sarah Carman Spring 2016" => { "25 Free" => 75.04, "50 Free" => 188.12, "25 Back" => 104.1 },
              }
            },
 tomas:     { name: "Tomas Carlino",    sex: "Male",   soo: "10139476",
@@ -266,14 +281,16 @@ tomas:     { name: "Tomas Carlino",    sex: "Male",   soo: "10139476",
              dob: "11/10/1996",
              results: {
                "Variety Village Fall 2014" => { "50 Free" => 56.84, "25 Back" => 30.19, "25 Breast" => 33.93, "25 Fly" => 45.88 },
-               "Mike Conrad Winter 2015" => {  },
                "Variety Village Spring 2015" => { "50 Free" => 55.62, "25 Back" => 26.69, "25 Breast" => 34.79, "25 Fly" => 32.22 },
                "Provincial Qualifier 2015" => { "25 Fly" => 32.63, "50 Free" => 53.54, "50 Back" => 57.31, "25 Back" => 24.45, "25 Breast" => 32.71 },
                "Mike Conrad Winter 2016" => { "50 Free" => 54.98, "25 Free" => 22.29, "25 Fly" => 28.06, "50 Back" => 57.19 },
+               "Sarah Carman Spring 2016" => { "50 Free" => 56.04, "25 Free" => 22.6, "50 Back" => 62.17, "25 Fly" => 31.75, "25 Back" => 28.64},
+               "Variety Village Spring 2016" => { "50 Free" => 50.51, "25 Free" => 22.49, "50 Fly" => 74.16, "50 Back" => 76.18 },
+               "Anne Ottenbrite 2016" => { "50 Free" => 48.89, "25 Free" => 22.14, "50 Fly" => 79.27, "50 Back" => 62.23, "25 Fly" => 31.87, "25 Back" => 25.14 },
              }
            },
 vishal:    { name: "Vishal Matekar",   sex: "Male",   soo: "10053200",
-             events: [ "25 Free", "50 Free", "25 Back", "50 Back" ],
+             events: [  ],
              pbs: { "25 Free" => 44, "50 Free" => 92, "25 Back" => 56, "50 Back" => 120 },
              dob: "15/01/1984",
              results: {
@@ -281,6 +298,7 @@ vishal:    { name: "Vishal Matekar",   sex: "Male",   soo: "10053200",
                "Mike Conrad Winter 2015" => { "25 Free" => 32.74, "50 Free" => 90.13, "25 Back" => 52.04, "50 Back" => 122.15 },
                "Variety Village Spring 2015" => { "25 Free" => 38.76, "50 Free" => 82.95, "25 Back" => 62.13, "50 Back" => 134.65 },
                "Mike Conrad Winter 2016" => { "25 Free" => 39.72, "50 Free" => 101.49, "25 Back" => 51.79, "50 Back" => 118.23 },
+               "Variety Village Spring 2016" => { "25 Free" => 41.21, "50 Free" => 95.5, "25 Back" => 57.27, "50 Back" => 114 }, # DQ in 25 Back and 50 Back
              }
            },
 }
@@ -393,4 +411,30 @@ meet_swimmers.each do |swimmer|
   #   row_string << time.to_s << ", "
   # end
   # puts row_string
+
+  ALL_EVENTS = [ "25 Free", "50 Free", "100 Free", "200 Free", "25 Back", "50 Back", "100 Back", "25 Breast", "50 Breast", "100 Breast", "25 Fly", "50 Fly", "100 IM" ]
+  ### History Report ***
+  roster.each do |key, swimmer_hash|
+    puts "*"*20
+    puts swimmer_hash[:name]
+    puts swimmer_hash[:sex]
+    puts swimmer_hash[:soo]
+    puts swimmer_hash[:dob]
+
+    results = swimmer_hash[:results]
+    puts "Meet, " + ALL_EVENTS.join(", ")
+    results.each do |meet_name, times|
+      row_string = "#{meet_name}, "
+      ALL_EVENTS.each do |event|
+        t = times[event]
+        if t
+          time = Time.at(t).utc.strftime("%-M:%S.%2N")
+          row_string << time << ", "
+        else
+          row_string << ", "
+        end
+      end
+      puts row_string[0..-3]
+    end
+  end
 end
